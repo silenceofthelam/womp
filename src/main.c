61a3bb6cceeb5dd4c45bbb6dfc9b6c603cbfff53
Dustin LaMiaux <silenceofthelamgithub@gmail.com>
2016-12-05 15:30:24 -0700

    Created main.c

---
+++ src/main.c
@@ -0,0 +1,29 @@
+/* The commented out lines
+are functions/operations that
+are to be implemented at a later date.
+They are more of a pseudocode than anything.
+*/
+
+
+int main(int argc, char** argv)
+{
+  // process_options;
+
+  // open(output);
+
+  /*while(!EOF)
+  {
+    readline
+
+    operate
+
+    print to output
+  } */
+
+  /*if(show_stats)
+  {
+    print_stats();
+  }*/
+
+  return 0;
+}
