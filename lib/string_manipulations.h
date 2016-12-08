2819674640cb7295d098817c1c254a1d2fbfca80
silenceofthelam <silenceofthelamgithub@gmail.com>
2016-12-08 11:06:20 -0700

    Added GNU copyright to the head of each file.

--- lib/string_manipulations.h
+++ lib/string_manipulations.h
@@ -2,7 +2,22 @@
   * Each function returns a pointer
   * to a new string. This new pointer
   * must be freed by the client.
-  *
+
+    Copyright (C) 2016 Dustin LaMIaux - SilenceOfTheLam 
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
   * @file string_manipulations.h
   */
 
