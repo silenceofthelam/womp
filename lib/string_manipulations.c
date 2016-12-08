7a723043b2a36a74068bde700e81f868b7ed88a6
silenceofthelam <silenceofthelamgithub@gmail.com>
2016-12-08 10:34:02 -0700

    Added check for malloc. Changed capitalize_nth_character structure.

--- lib/string_manipulations.c
+++ lib/string_manipulations.c
@@ -28,21 +28,31 @@ char * concatenate(char *first_word, char *second_word)
 
 char * capitalize_nth_character(char *original_word, int character_to_capitalize)
 {
-	char *new_word = (char *)malloc(strlen(original_word) + 1);
+	char *new_word;
 	int i;
+  
+  new_word = malloc(strlen(original_word) + 1);
+  
+  if(new_word != NULL)
+  {
+    for(i = 0; i < strlen(original_word); i++)
+    {
+      if(i == (character_to_capitalize - 1))
+      {
+        if(original_word[i] >= 'a' && original_word[i] <= 'z')
+        {
+          new_word[i] = original_word[i] - ('a' - 'A');
+        }
+      }
+      else
+      {
+        new_word[i] = original_word[i];
+      }
+    }
 
-	for(i = 0; i < strlen(original_word); i++)
-	{
-		new_word[i] = original_word[i];
+    new_word[i] = '\0';
+  }
 
-		if(i == (character_to_capitalize - 1))
-		{
-			new_word[i] = new_word[i] >= 'a' ? 
-				(new_word[i] - ('a' - 'A')) : new_word[i];
-		}
-	}
-
-	new_word[i] = '\0';
 
 	return new_word;
 }
